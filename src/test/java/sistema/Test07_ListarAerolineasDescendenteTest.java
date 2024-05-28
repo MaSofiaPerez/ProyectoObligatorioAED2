e5686bcee8dfd0115d96fed9281db322a58ece5b
Marcos24001 <147960931+Marcos24001@users.noreply.github.com>
2024-05-27 22:59:28 -0300

    Update Test07_ListarAerolineasDescendenteTest.java

--- src/test/java/sistema/Test07_ListarAerolineasDescendenteTest.java
+++ src/test/java/sistema/Test07_ListarAerolineasDescendenteTest.java
@@ -27,12 +27,29 @@ public class Test07_ListarAerolineasDescendenteTest {
 
         retorno = sistema.registrarAerolinea(new String("D"), new String("Aerolinea 4"));
         assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+        retorno = sistema.registrarAerolinea(new String("E"), new String("Aerolinea 5"));
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+
+        retorno = sistema.registrarAerolinea(new String("F"), new String("Aerolinea 6"));
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+
+        retorno = sistema.registrarAerolinea(new String("G"), new String("Aerolinea 7"));
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+
+        retorno = sistema.registrarAerolinea(new String("H"), new String("Aerolinea 8"));
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+
+        retorno = sistema.registrarAerolinea(new String("I"), new String("Aerolinea 9"));
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
+
+        retorno = sistema.registrarAerolinea(new String("J"), new String("Aerolinea 10"));
+        assertEquals(Retorno.Resultado.OK, retorno.getResultado());
     }
 
     @Test
-    void listarAscendente() {
+    void listarDescendente() {
         retorno = sistema.listarAerolineasDescendente();
-        String resEsperado = "D;Aerolinea 4|C;Aerolinea 3|B;Aerolinea 2|A;Aerolinea 1";
+        String resEsperado = "J;Aerolinea 10|I;Aerolinea 9|H;Aerolinea 8|G;Aerolinea 7|F;Aerolinea 6|E;Aerolinea 5|D;Aerolinea 4|C;Aerolinea 3|B;Aerolinea 2|A;Aerolinea 1";
         assertEquals(resEsperado, retorno.getValorString());
     }
 
