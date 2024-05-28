5827d1a6c2bf66e3cf819c32264e4fc1f5f5f39e
Marcos24001 <147960931+Marcos24001@users.noreply.github.com>
2024-05-27 23:06:58 -0300

    Update Test10_RegistrarVueloTest.java

--- src/test/java/sistema/Test10_RegistrarVueloTest.java
+++ src/test/java/sistema/Test10_RegistrarVueloTest.java
@@ -17,7 +17,7 @@ public class Test10_RegistrarVueloTest {
     }
 
     @Test
-    void noDeberiaRegistrarVueloConParamsIgualesACero() {
+  void noDeberiaRegistrarVueloConParamsIgualesACero() {
         retorno = sistema.registrarVuelo(new String("1"),new String("2"),new String("1"),0,10,100,new String("1"));
         assertEquals(Retorno.Resultado.ERROR_1,retorno.getResultado());
 
@@ -74,12 +74,24 @@ public class Test10_RegistrarVueloTest {
     void noDeberiaRegistrarVueloSiAeropuertoOrigenNoExiste() {
         retorno = sistema.registrarVuelo(new String("1"),new String("2"),new String("1"),10,10,100,new String("1"));
         assertEquals(Retorno.Resultado.ERROR_3,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("32"),new String("2"),new String("14"),140,103,145,new String("14"));
+        assertEquals(Retorno.Resultado.ERROR_3,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("331"),new String("1"),new String("31"),1033,1044,13300,new String("31"));
+        assertEquals(Retorno.Resultado.ERROR_3,retorno.getResultado());
     }
 
     @Test
     void noDeberiaRegistrarVueloSiAeropuertoDestinoNoExiste() {
         sistema.registrarAeropuerto(new String("1"), new String("Aeropuerto1"));
+        sistema.registrarAeropuerto(new String("4"), new String("Aeropuerto4"));
+        sistema.registrarAeropuerto(new String("6"), new String("Aeropuerto6"));
         retorno = sistema.registrarVuelo(new String("1"),new String("2"),new String("1"),10,10,100,new String("1"));
+        assertEquals(Retorno.Resultado.ERROR_4,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("4"),new String("22"),new String("33"),12,144,14,new String("12"));
+        assertEquals(Retorno.Resultado.ERROR_4,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("4"),new String("52"),new String("55"),144,12,155,new String("14"));
+        assertEquals(Retorno.Resultado.ERROR_4,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("6"),new String("12"),new String("77"),13,155,123,new String("17"));
         assertEquals(Retorno.Resultado.ERROR_4,retorno.getResultado());
     }
 
@@ -87,7 +99,13 @@ public class Test10_RegistrarVueloTest {
     void noDeberiaRegistrarVueloSiAerolineaNoExiste() {
         sistema.registrarAeropuerto(new String("1"), new String("Aeropuerto1"));
         sistema.registrarAeropuerto(new String("2"), new String("Aeropuerto2"));
+        sistema.registrarAeropuerto(new String("4"), new String("Aeropuerto4"));
+        sistema.registrarAeropuerto(new String("6"), new String("Aeropuerto6"));
         retorno = sistema.registrarVuelo(new String("1"),new String("2"),new String("1"),10,10,100,new String("1"));
+        assertEquals(Retorno.Resultado.ERROR_5,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("4"),new String("6"),new String("55"),144,12,155,new String("14"));
+        assertEquals(Retorno.Resultado.ERROR_5,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("6"),new String("4"),new String("77"),13,155,123,new String("17"));
         assertEquals(Retorno.Resultado.ERROR_5,retorno.getResultado());
     }
 
@@ -104,11 +122,27 @@ public class Test10_RegistrarVueloTest {
     void noDeberiaRegistrarVueloSiYaExiste() {
         sistema.registrarAeropuerto(new String("1"), new String("Aeropuerto1"));
         sistema.registrarAeropuerto(new String("2"), new String("Aeropuerto2"));
+        sistema.registrarAeropuerto(new String("3"), new String("Aeropuerto3"));
+        sistema.registrarAeropuerto(new String("7"), new String("Aeropuerto7"));
         sistema.registrarAerolinea("1", "Aerolinea 1");
+        sistema.registrarAerolinea("2", "Aerolinea 2");
         sistema.registrarConexion(new String("1"), new String("2"),10);
+        sistema.registrarConexion(new String("2"), new String("7"),23);
+        sistema.registrarConexion(new String("3"), new String("1"),46);
+        sistema.registrarConexion(new String("2"), new String("3"),12);
+
         sistema.registrarVuelo(new String("1"),new String("2"),new String("1"),10,10,100,new String("1"));
+        sistema.registrarVuelo(new String("2"),new String("7"),new String("654"),30,14,154,new String("1"));
+        sistema.registrarVuelo(new String("3"),new String("1"),new String("22"),13,44,132,new String("2"));
+        sistema.registrarVuelo(new String("2"),new String("3"),new String("33"),34,22,144,new String("2"));
 
         retorno = sistema.registrarVuelo(new String("1"),new String("2"),new String("1"),10,10,100,new String("1"));
+        assertEquals(Retorno.Resultado.ERROR_7,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("2"),new String("7"),new String("654"),30,14,154,new String("1"));
+        assertEquals(Retorno.Resultado.ERROR_7,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("3"),new String("1"),new String("22"),13,44,132,new String("2"));
+        assertEquals(Retorno.Resultado.ERROR_7,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("2"),new String("3"),new String("33"),34,22,144,new String("2"));
         assertEquals(Retorno.Resultado.ERROR_7,retorno.getResultado());
     }
 
@@ -116,9 +150,38 @@ public class Test10_RegistrarVueloTest {
     void deberiaRegistrarVuelo() {
         sistema.registrarAeropuerto(new String("1"), new String("Aeropuerto1"));
         sistema.registrarAeropuerto(new String("2"), new String("Aeropuerto2"));
+        sistema.registrarAeropuerto(new String("3"), new String("Aeropuerto3"));
+        sistema.registrarAeropuerto(new String("4"), new String("Aeropuerto4"));
+        sistema.registrarAeropuerto(new String("5"), new String("Aeropuerto5"));
+        sistema.registrarAeropuerto(new String("6"), new String("Aeropuerto6"));
+        sistema.registrarAeropuerto(new String("7"), new String("Aeropuerto7"));
+        sistema.registrarAeropuerto(new String("8"), new String("Aeropuerto8"));
+
         sistema.registrarAerolinea("1", "Aerolinea 1");
+        sistema.registrarAerolinea("2", "Aerolinea 2");
+        sistema.registrarAerolinea("3", "Aerolinea 3");
+        sistema.registrarAerolinea("4", "Aerolinea 4");
+
         sistema.registrarConexion(new String("1"), new String("2"),10);
+        sistema.registrarConexion(new String("3"), new String("4"), 100);
+        sistema.registrarConexion(new String("4"), new String("5"), 200);
+        sistema.registrarConexion(new String("5"), new String("6"), 150);
+
         retorno = sistema.registrarVuelo(new String("1"),new String("2"),new String("1"),10,10,100,new String("1"));
+        assertEquals(Retorno.Resultado.OK,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("4"), new String("5"), new String("55"), 100, 50, 200, new String("4"));
+        assertEquals(Retorno.Resultado.OK,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("4"), new String("5"), new String("445"), 100, 30, 500, new String("4"));
+        assertEquals(Retorno.Resultado.OK,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("4"), new String("5"), new String("5"), 250, 60, 700, new String("4"));
+        assertEquals(Retorno.Resultado.OK,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("5"), new String("6"), new String("2"), 150, 60, 250, new String("2"));
+        assertEquals(Retorno.Resultado.OK,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("3"), new String("4"), new String("3"), 120, 55, 220, new String("3"));
+        assertEquals(Retorno.Resultado.OK,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("3"), new String("4"), new String("12"), 120, 55, 220, new String("4"));
+        assertEquals(Retorno.Resultado.OK,retorno.getResultado());
+        retorno = sistema.registrarVuelo(new String("1"), new String("2"), new String("12"), 220, 15, 440, new String("4"));
         assertEquals(Retorno.Resultado.OK,retorno.getResultado());
     }
 }
